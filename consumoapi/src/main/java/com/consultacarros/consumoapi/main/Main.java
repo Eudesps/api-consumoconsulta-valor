d0b43729552cadd11a086434788263e7a0e9d816
José Eudes <joseeudes969@gmail.com>
2026-01-14 15:59:16 -0300

    requisição básica Json

--- consumoapi/src/main/java/com/consultacarros/consumoapi/main/Main.java
+++ consumoapi/src/main/java/com/consultacarros/consumoapi/main/Main.java
@@ -1,12 +1,27 @@
 package com.consultacarros.consumoapi.main;
 
+import com.consultacarros.consumoapi.service.ConsumoAPI;
+
+import java.util.Scanner;
+
 public class Main {
+    private final String ENDERECO = "https://parallelum.com.br/fipe/api/v1/";
+
+    Scanner scanner = new Scanner(System.in);
+    ConsumoAPI consumoAPI = new ConsumoAPI();
 
     public void exibirMenu(){
 
         System.out.println("******************************** \n" +
                 "INFORMAÇÕES SOBRE O PREÇO MÉDIO DE AUTOMÓVEIS \n" +
-                "*** Escolha o se é \"carro\", \"moto\" ou \"caminhão\" ***");
+                "*** Escolha o se é \"carros\", \"motos\" ou \"caminhoes\" ***");
+
+        var tipoAutomovel = scanner.next().toLowerCase();
+
+        String json = consumoAPI.obterDados(ENDERECO + tipoAutomovel + "/marcas/");
+
+        System.out.println(json);
+
 
     }
 }
