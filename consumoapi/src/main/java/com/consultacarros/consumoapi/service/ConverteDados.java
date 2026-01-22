e18b27d57a746865561452c661bda69f6f6fb60f
José Eudes <joseeudes969@gmail.com>
2026-01-22 15:19:51 -0300

    atualização de método de converter dados

--- consumoapi/src/main/java/com/consultacarros/consumoapi/service/ConverteDados.java
+++ consumoapi/src/main/java/com/consultacarros/consumoapi/service/ConverteDados.java
@@ -2,20 +2,32 @@ package com.consultacarros.consumoapi.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.type.CollectionType;
 
 import java.util.List;
 
-public class ConverteDados {
+public class ConverteDados implements IConverteDados {
     private ObjectMapper mapper = new ObjectMapper();
 
     //como a classe é generica, é interessante na hora de chamar o método, passar também a classe que se deseja
     //converter no momento de sua utilização.
-    public <T> List<T> converteLista(String json, Class<T> classe) {
+
+    @Override
+    public <T> T converteDados(String json, Class<T> classe){
         try {
-            return mapper.readValue(
-                    json,
-                    mapper.getTypeFactory().constructCollectionType(List.class, classe)
-            );
+            return mapper.readValue(json, classe);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public <T> List<T> obterLista(String json, Class<T> classe) {
+        CollectionType lista = mapper.getTypeFactory()
+                .constructCollectionType(List.class, classe);
+
+        try {
+            return mapper.readValue(json, lista);
         } catch (JsonProcessingException e) {
             throw new RuntimeException(e);
         }
--- consumoapi/src/main/java/com/consultacarros/consumoapi/service/IConverteDados.java
+++ consumoapi/src/main/java/com/consultacarros/consumoapi/service/IConverteDados.java
@@ -2,6 +2,10 @@ package com.consultacarros.consumoapi.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 
+import java.util.List;
+
 public interface IConverteDados {
     <T> T converteDados(String json, Class<T> classe) throws JsonProcessingException;
+
+    <T> List<T> obterLista (String json, Class<T> classe);
 }
